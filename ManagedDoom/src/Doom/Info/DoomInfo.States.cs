8e1ea34c576a27171152f86a901403b4cfdccb5c
Seth Kimberlin <sethkimberlin04@gmail.com>
2025-03-10 16:09:50 -0600

    Added Dog Enemy

--- ManagedDoom/src/Doom/Info/DoomInfo.States.cs
+++ ManagedDoom/src/Doom/Info/DoomInfo.States.cs
@@ -1007,6 +1007,22 @@ namespace ManagedDoom
             new MobjStateDef(977, Sprite.TROO, 5, 8, null, ma.FaceTarget, MobjState.ZombieAtk3, 0, 0), // State.ZombieAtk2
             new MobjStateDef(978, Sprite.TROO, 6, 6, null, ma.ZombieAttack, MobjState.ZombieRun1, 0, 0), // State.ZombieAtk3
             new MobjStateDef(979, Sprite.MXAM, 0, -1, null, null, MobjState.Null, 0, 0), // State.MaxAmmo
+            new MobjStateDef(980, Sprite.INSK, 0, -1, null, null, MobjState.Null, 0, 0), // State.InstaKill
+            new MobjStateDef(981, Sprite.DBPT, 0, -1, null, null, MobjState.Null, 0, 0), // State.DoublePoints
+            new MobjStateDef(982, Sprite.NUKE, 0, -1, null, null, MobjState.Null, 0, 0), // State.Nuke
+            new MobjStateDef(983, Sprite.SARG, 0, 10, null, ma.ZombieLook, MobjState.DogStnd2, 0, 0), // State.DogStnd
+            new MobjStateDef(984, Sprite.SARG, 1, 10, null, ma.ZombieLook, MobjState.DogStnd, 0, 0), // State.DogStnd2
+            new MobjStateDef(985, Sprite.SARG, 0, 2, null, ma.ZombieChase, MobjState.DogRun2, 0, 0), // State.DogRun1
+            new MobjStateDef(986, Sprite.SARG, 0, 2, null, ma.ZombieChase, MobjState.DogRun3, 0, 0), // State.DogRun2
+            new MobjStateDef(987, Sprite.SARG, 1, 2, null, ma.ZombieChase, MobjState.DogRun4, 0, 0), // State.DogRun3
+            new MobjStateDef(988, Sprite.SARG, 1, 2, null, ma.ZombieChase, MobjState.DogRun5, 0, 0), // State.DogRun4
+            new MobjStateDef(989, Sprite.SARG, 2, 2, null, ma.ZombieChase, MobjState.DogRun6, 0, 0), // State.DogRun5
+            new MobjStateDef(990, Sprite.SARG, 2, 2, null, ma.ZombieChase, MobjState.DogRun7, 0, 0), // State.DogRun6
+            new MobjStateDef(991, Sprite.SARG, 3, 2, null, ma.ZombieChase, MobjState.DogRun8, 0, 0), // State.DogRun7
+            new MobjStateDef(992, Sprite.SARG, 3, 2, null, ma.ZombieChase, MobjState.DogRun1, 0, 0), // State.DogRun8
+            new MobjStateDef(993, Sprite.SARG, 4, 8, null, ma.FaceTarget, MobjState.DogAtk2, 0, 0), // State.DogAtk1
+            new MobjStateDef(994, Sprite.SARG, 5, 8, null, ma.FaceTarget, MobjState.DogAtk3, 0, 0), // State.DogAtk2
+            new MobjStateDef(995, Sprite.SARG, 6, 8, null, ma.ZombieAttack, MobjState.DogRun1, 0, 0), // State.DogAtk3
         };
     }
 }
